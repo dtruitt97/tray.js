f352f15110060de6a2db6584f962915b5ce5c385
dtruitt97 <d.truitt214@gmail.com>
2015-08-20 21:09:45 -0400

    Extended features and increased usability.
    Configured the script to move the inactive tray to avoid the clunky
    z-index method of handling overlaps on displays smaller than the sum of
    the tray sizes. Added a couple other little features that no one will
    notice yet.

--- tray.js
+++ tray.js
@@ -1,48 +1,82 @@
 var animationlength = 25;
 var trays = {
-    left: 512,
+    left: 256,
     moveleft: "off",
     movecenter: "on",
     right: 512,
     moveright: "off",
-    fixnav: "on"
+    fixnav: "off"
 };
 var action = {};                                 
 var segments = [];
+var spare;
+var animationinitialized = false;
 
 function initializetrays() {
     if (trays.left != 0) {
         trays.left = Math.min(trays.left, window.innerWidth);
-        if (trays.moveleft == "on") {
-            segments[0] = document.querySelector("#left-tray");
-        } else {
-            segments[0] = {style: {}};
-        }
+        segments[0] = document.querySelector("#left-tray");
+        
+        segments[0].style.position = "fixed";
         segments[0].style.left = "-" + trays.left + "px";
+        segments[0].style.top = "0px";
         segments[0].style.width = trays.left + "px";
+        segments[0].style.zIndex = "-2";
+        
+        if (trays.moveleft == "off") {
+            segments[0].style.left = "0px";
+            segments[0] = {style: {}};
+            segments[0].style.left = "-" + trays.left + "px";
+            segments[0].style.width = trays.left + "px";
+        }
     }
     if (trays.movecenter == "on") {
         segments[1] = document.querySelector(".center-tray");
+        segments[1].style.position = "absolute";
         segments[1].style.left = "0px";
+        segments[1].style.top = "0px";
         segments[1].style.width = window.innerWidth + "px";
     }
     if (trays.right != 0) {
         trays.right = Math.min(trays.right, window.innerWidth);
-        if (trays.moveright == "on") {
-            segments[2] = document.querySelector("#right-tray");
-        } else {
-            segments[2] = {style: {}};
-        }
+        segments[2] = document.querySelector("#right-tray");
+        
+        segments[2].style.position = "fixed";
         segments[2].style.left = window.innerWidth + "px";
+        segments[2].style.top = "0px";
         segments[2].style.width = trays.right + "px";
+        segments[2].style.zIndex = "-2";
+        
+        if (trays.moveright == "off") {
+            segments[2].style.left = window.innerWidth - trays.right + "px";
+            segments[2] = {style: {}};
+            segments[2].style.left = window.innerWidth + "px";
+            segments[2].style.width = trays.right + "px";
+        }
     }
     if (trays.fixnav == "on") {
         segments[3] = document.querySelector("header");
         segments[3].style.left = "0px";
     }
+    
+    spare = segments.length;
 }
 
 function animatetray() {
+    if (animationinitialized == false){
+        if (action.index == 0 && trays.moveleft == "off") {
+            document.querySelector("#left-tray").style.left = "0px";
+            segments[spare] = document.querySelector("#right-tray");
+        }
+
+        if (action.index == 2 && trays.moveright == "off") {
+            document.querySelector("#right-tray").style.left = window.innerWidth - trays.right + "px";
+            segments[spare] = document.querySelector("#left-tray");
+        }
+        
+        animationinitialized = true;
+    }
+    
     var current = [];
     var next = [];
     
@@ -66,6 +100,8 @@ function animatetray() {
             next[i] = current[i] + difference;
             segments[i].style.left = next[i] + "px";
         }
+        
+        animationinitialized = false;
     }
 }
 
@@ -79,7 +115,6 @@ var closeright = document.querySelector("#close-right-tray");
 
 if (openleft != null) {
     openleft.addEventListener("click", function(){
-        document.querySelector("#left-tray").style.zIndex = "-1";
         action = {
             index: 0,
             target: 0,
@@ -91,7 +126,6 @@ if (openleft != null) {
 
 if (closeleft != null) {
     closeleft.addEventListener("click", function(){
-        document.querySelector("#left-tray").style.zIndex = "-2";
         action = {
             index: 0,
             target: -trays.left,
@@ -103,7 +137,6 @@ if (closeleft != null) {
 
 if (openright != null) {
     openright.addEventListener("click", function(){
-        document.querySelector("#right-tray").style.zIndex = "-1";
         action = {
             index: 2,
             target: window.innerWidth - trays.right,
@@ -115,7 +148,6 @@ if (openright != null) {
 
 if (closeright != null) {
     closeright.addEventListener("click", function(){
-        document.querySelector("#right-tray").style.zIndex = "-2";
         action = {
             index: 2,
             target: window.innerWidth,
